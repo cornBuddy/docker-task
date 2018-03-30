ebb872ac858b148810708d12f423788d1256732c
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2018-03-30 15:22:59 +0300

    chore(docker): run nginx container on 80 port

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ docker network create --subnet=192.168.0.0/16 tomcat-net
 
 nginx_src="$(pwd)/nginx-conf"
 docker run --rm -d --network=tomcat-net --ip 192.168.1.20 "$tomcat_id"
-docker run -d --network=tomcat-net --ip 192.168.1.10 -p 8080:80 \
+sudo docker run --rm -d --network=tomcat-net --ip 192.168.1.10 -p 80:80 \
     --mount type=bind,src="$nginx_src/tomcat.conf",dst=/etc/nginx/conf.d/tomcat.conf,readonly \
     --mount type=bind,src="$nginx_src/nginx.conf",dst=/etc/nginx/nginx.conf,readonly \
     "$nginx_id"
