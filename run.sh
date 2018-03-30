9eaa20472253e90536f8300dbca2cefe55da8c32
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2018-03-30 17:50:38 +0300

    chore(docker): add jenkins container

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# PART 1
 tomcat_id=$(docker build -q -f tomcat.Dockerfile .)
 nginx_id=$(docker build -q -f nginx.Dockerfile .)
 
@@ -11,3 +12,10 @@ sudo docker run --rm -d --network=tomcat-net --ip 192.168.1.10 -p 80:80 \
     --mount type=bind,src="$nginx_src/tomcat.conf",dst=/etc/nginx/conf.d/tomcat.conf,readonly \
     --mount type=bind,src="$nginx_src/nginx.conf",dst=/etc/nginx/nginx.conf,readonly \
     "$nginx_id"
+
+# PART 2
+mkdir -p jenkins-home
+docker pull jenkins:alpine
+docker run -p 8080:8080 -p 50000:50000 -d \
+    -v  "$(pwd)/jenkins-home:/var/jenkins_home" \
+    jenkins:alpine
